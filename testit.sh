eb2cc30f65e8a61f08214485fd309873775445d4
tcramm0nd <25987791+tcramm0nd@users.noreply.github.com>
2022-11-25 21:41:10 -0500

    Testing: pdated coverage

---
+++ testit.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# get working dir
+# get token for upload
+# save to file
+# upload file
+
+xml () {
+  # Generate XML Reports of coverage with pytest
+  echo "XML Coverage Testing"
+  pytest \
+    --durations=0 \
+    --cov-report xml:cov.xml \
+    --cov=strava_map .
+}
+
+default() {
+    echo "Default Coverage Testing"
+    # Default test with results to the terminal.
+  pytest \
+    --durations=0 \
+    --cov-report term-missing \
+    --cov=strava_map .
+}
+if test $# -eq 0
+then
+  default
+  exit 0
+fi
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "heeeelp"
+      shift
+      ;;
+    -x|--xml)
+      xml
+      echo " "
+      echo "Created XML report"
+      echo " "
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+# python-codacy-coverage -r cov.xml
