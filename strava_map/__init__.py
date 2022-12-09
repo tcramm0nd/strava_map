690d5d9d77327dd0d6402335812a6b95c6345528
tcramm0nd <25987791+tcramm0nd@users.noreply.github.com>
2022-12-09 15:20:50 -0500

    Created ROI class, cleanup

--- strava_map/__init__.py
+++ strava_map/__init__.py
@@ -1,6 +1,7 @@
-from .map import Map
-from .activities import ActivityDB
-from .explorer import Explorer
 """Download or import activities to an ActivityDB, and then generate a Map using
     an ActivityDB"""
+from .map import Map
+from .activities import ActivityDB
+from .explorer import Explorer, ROI
+
 __version__ = "0.0.5" 
